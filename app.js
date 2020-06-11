a7dc9f4be4e70bf7a11e0d5037174d5f89467fa6
Mohammed Shoaib <shoaibbilal101@gmail.com>
2020-06-11 20:42:55 +0500

    123

--- app.js
+++ app.js
@@ -352,3 +352,152 @@ console.log("Hello...I can run Js through my webbrowser console")
 // document.write("Total Subject  Marks: " + total + "<br/>")
 
 // document.write(percentage1 + percentage2 + percentage3 + "<br/>")
+
+
+// ch 9-11
+// USER INPUT & CONDITIONAL STATEMENT
+
+// 1. Write a program to take “city” name as input from user. If user enters “Karachi”, welcome the user like this: “Welcome to city of lights” 
+
+// var city = prompt("Enter City Name: ")
+// if (city === "karachi") {
+//     alert("Welcome to city of lights")
+// }
+
+// 2. Write a program to take “gender” as input from user. If the user is male, give the message: Good Morning Sir. If the user is female, give the message: Good Morning Ma’am.
+
+// var gender = prompt("Enter GemderL ")
+// if (gender === "male") {
+//     alert(" Good Morning Sir")
+// } else {
+//     alert("Good Morning Ma’am")
+// }
+
+// 3. Write a program to take input color of road traffic signal from the user & show the message according to this table: 
+
+// var color = prompt("Enter Signal color: ")
+// if (color === "Red") {
+//     alert("Must Stop ")
+// } else if (color === "Yellow") {
+//     alert("Ready to move  ")
+// } else if (color === "Green") {
+//     alert("Move Now ")
+// }
+
+// 4. Write a program to take input remaining fuel in car (in litres) from user. If the current fuel is less than 0.25litres, show the message “Please refill the fuel in your car”
+
+// var fuel = prompt("Enter remaining fuel in car ")
+// if (fuel === "0.25") {
+//     alert("Please refill the fuel in your car")
+// }    
+
+// 5. Run this script, & check whether alert message would be displayed or not. Record the outputs. 
+
+// var a = 4;
+// if (++a === 5) {
+//     alert("given condition for variable a is true");
+// }
+// var b = 82;
+// if (b++ === 83) { alert("given condition for variable b is true"); }
+
+// var c = 12;
+// if (c++ === 13) { alert("condition 1 is true"); }
+// if (c === 13) { alert("condition 2 is true"); }
+// if (++c < 14) { alert("condition 3 is true"); }
+// if (c === 14) { alert("condition 4 is true"); }
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost) { alert("The cost equals"); }
+
+// if (true) {
+//     alert("True");
+// }
+// if (false) {
+//     alert("False");
+// }
+// if ("car" < "cat") {
+//     alert("car is smaller than cat");
+// }
+
+
+// 6. Write a program to take input the marks obtained in three subjects & total marks. Compute & show the resulting percentage on your page. Take percentage & compute grade as per following table:
+
+// var percent = +prompt("Enter Your percentage")
+// if (percent >= 80 && percent <= 100) {
+//     alert("A+")
+// } else if (percent >= 70 && percent <= 80) {
+//     alert("A")
+// } else if (percent >= 60 && percent <= 70) {
+//     alert("B")
+// } else if (percent >= 50 && percent <= 60) {
+//     alert("C")
+// } else if (percent >= 40 && percent <= 50) {
+//     alert("D")
+// } else if (percent >= 33 && percent <= 40) {
+//     alert("E")
+// } else if (percent >= 0 && percent <= 33) {
+//     alert("F")
+// } else {
+//     alert("You didnot write correct percentage")
+// }
+
+// 7. Guess game: Store a secret number (ranging from 1 to 10) in a variable. Prompt user to guess the secret number. a. If user guesses the same number, show “Bingo! Correct answer”. b. If the guessed number +1 is the secret number, show “Close enough to the correct answer”.
+// var secret = 5
+// var number = prompt("Enter guess  secret number ranging from 1 to 10")
+// if (number === "secret") {
+//     alert("Bingo! Correct answer”")
+// }
+// } else if ((secret - number) === 1) {
+//     alert("Close enough to the correct answer")
+// }
+
+// 8. Write a program to check whether the given number is divisible by 3. Show the message to the user if the number is divisible by 3.
+
+// var number = prompt("Enter Number Here! ")
+// if (number % 3 === 0) {
+//     alert("odd number")
+// }
+
+// // 9. Write a program that checks whether the given input is an even number or an odd number. 
+
+// var number = prompt("Enter Number Here! ")
+// if (number % 2 === 0) {
+//     alert("Even number")
+// }
+
+// 10. Write a program that takes temperature as input and shows a message based on following criteria a. T > 40 then “It is too hot outside.” b. T > 30 then “The Weather today is Normal.” c. T > 20 then “Today’s Weather is cool.” d. T > 10 then “OMG! Today’s weather is so Cool.”
+
+// var t = prompt("Enter Temperature: ")
+// if (t === "40") {
+//     alert("It is too hot outside")
+// } else if (t === "30") {
+//     alert("The Weather today is Normal")
+// } else if (t === "20") {
+//     alert("Today’s Weather is cool")
+// } else if (t === "10") {
+//     alert("OMG! Today’s weather is so Cool")
+// }
+
+// 11. Write a program to create a calculator for +,-,*, / & % using if statements. Take the following input: a. First number b. Second number c. Operation (+, -, *, /, %) Compute & show the calculated result to user. 
+
+// Calculator
+
+// var val1 = prompt("Enter Your First Value: ")
+// var sign = prompt("Enter Your operator : ")
+// var val2 = prompt("Enter Your Second Value: ")
+
+
+// console.log(val1 + sign + val2)
+// if (sign === '+') {
+//     alert((+val1) + (+val2))
+// } else if (sign === '-') {
+//     alert(val1 - val2)
+// } else if (sign === '*') {
+//     alert(val1 * val2)
+// } else if (sign === '/') {
+//     alert(val1 / val2)
+// } else if (sign === '%') {
+//     alert(val1 / val2 * 100 + "%")
+// }
